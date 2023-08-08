d2b7bea4889f949e95e1a9d362cfbc09bf63d7c3
gajjar96019 <gajjar96019@gamil.com>
2023-08-08 15:11:39 +0530

    Jasonholder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
+
+  const [val, setval] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setval(data))
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Container fluid>
+        <Row>
+          {
+            val.map((item) => {
+              return (
+                <Col lg='6' className='g-3'>
+                  <div className='d-flex border shadow'>
+                    <img src={require(`./images/1.jpg`)} width={200} />
+                    <div className=''>
+                      <p>{item.id}</p>
+                      <p>{item.title}</p>
+                      <p>{item.body}</p>
+                    </div>
+                  </div>
+                </Col >
+              )
+            })
+          }
+        </Row>
+      </Container>
     </div>
   );
 }
